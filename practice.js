6cf70a10b068383328ae92225e4fb051ca2c8afb
kylieanyce <kylieanyce@gmail.com>
2021-01-21 16:30:08 -0600

    worked on first practice function - Best In Show

---
+++ practice.js
@@ -0,0 +1,14 @@
+//----------------Best In Show -------------------------------
+
+const favoriteBreed = (dogtype) => {
+    if (dogtype === "schnauzer") {
+        return " my favorite dog breed is " + dogtype
+    } else if (dogtype === "meow") {
+        return "I like cats"
+    }
+}
+console.log("when it comes to pets" + favoriteBreed("meow"))
+
+//--------------------Addition -------------------------------
+
+
